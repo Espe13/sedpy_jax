b9fd1fff6d2605bbb623a6e9f54180e66ad305ba
Espe13 <42141549+Espe13@users.noreply.github.com>
2025-04-11 12:03:54 +0300

    Change structure into sub directory

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='sedpy_jax',
     version='0.1.0',
-    description='JAX-optimized SED fitting tools inspired by sedpy',
-    author='Ben Johnson, jaxified by Amanda Stoffers',
-    packages=find_packages(),
+    description='JAX-accelerated version of SEDpy by Ben Johnson',
+    author='Amanda Stoffers',
+    packages=find_packages(where="."), 
     include_package_data=True,
     install_requires=[
         'jax',
         'matplotlib',
         'numpy',
         'scipy',
-        # optionally rich, astropy, etc
     ],
-    classifiers=[
+        classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
