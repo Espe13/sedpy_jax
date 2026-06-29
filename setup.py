ed6239aaedc8f868ef333420ced3765705568d7f
Espe13 <42141549+Espe13@users.noreply.github.com>
2026-06-29 14:17:55 +0100

    Release 0.1.1: inres + LOSVD wrap-around fix; PyPI metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='sedpy_jax',
-    version='0.1.0',
-    description='JAX-accelerated version of SEDpy by Ben Johnson',
+    version='0.1.1',
+    description='JAX-accelerated version of sedpy (filter projections + spectral smoothing) by Ben Johnson',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Amanda Stoffers',
-    packages=find_packages(where="."), 
+    author_email='aas208@cam.ac.uk',
+    url='https://github.com/Espe13/sedpy_jax',
+    license='MIT',
+    packages=find_packages(where="."),
     include_package_data=True,
+    python_requires='>=3.10',
     install_requires=[
         'jax',
         'matplotlib',
         'numpy',
         'scipy',
     ],
-        classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Astronomy",
     ],
 )
